89f8f4d58476752e8ad9ce39c333e2c749731988
iamjoaomarcos <work.iamjoaomarcos@gmail.com>
2025-08-15 11:00:11 +0900

    added a login text

---
+++ login.php
@@ -0,0 +1,7 @@
+<?php
+echo "Hello, Github!";
+echo "Adding a new line to the file.";
+echo "Added git ignore file.";
+echo "another one";
+echo "This is a new line in the index file.";
+echo "login is a just a file name.";
